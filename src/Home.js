f8dd66db9c55dc18df4f36741246f3411b6a8be9
Kieran B <kieranbru.web@gmail.com>
2021-08-10 11:02:46 -0400

    Updated navbar bug

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 class Home extends Component {
     componentDidMount() {
-        if (this.props.switch()) {
+        if (this.props.state) {
             this.props.switch()
         }
 
